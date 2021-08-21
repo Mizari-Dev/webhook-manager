b4bcad0a4786ea9e808ecfdcb4ec7132596813b6
Mizari-W <warriorofmeadow@gmail.com>
2021-08-21 21:21:16 +0200

    commit

--- main.js
+++ main.js
@@ -5,55 +5,57 @@ const path = require('path');
 // importation du package "fs" pour lire le fichier json
 const fs = require('fs');
 
-// function checkWebhook() {
-//   var res = false;
-//
-//   fs.readFile('webhooks-stack.json', async (err, data) => {
-//     if (err) res = "error";
-//
-//     let json = await JSON.parse(data);
-//     if (json.length > 0) {
-//       res = true;
-//     }
-//   });
-//
-//   return res;
-// }
-//
-// console.log(checkWebhook());
+fs.readFile('webhooks-stack.json', 'utf-8', (err, data) => {
+  if (err) res = "error";
 
-// fonction de création de la fenêtre
-function createWindow () {
-  // nouvel objet fenêtre
-  const win = new BrowserWindow({
-    minWidth: 800,
-    minHeight: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    },
-    icon: path.join(__dirname, 'assets/W.A.C.ico')
+  var htmlToLoad = "index.html";
+  try {
+    let list = JSON.parse(data);
+    if (typeof list === "object") {
+      if (list.length > 0) {
+        htmlToLoad = "manager.html";
+      }
+    } else {
+      htmlToLoad = "error.html";
+    }
+  } catch (e) {
+    htmlToLoad = "error.html";
+  }
+
+  // fonction de création de la fenêtre
+  function createWindow () {
+    // nouvel objet fenêtre
+    const win = new BrowserWindow({
+      minWidth: 952,
+      minHeight: 526,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      },
+      icon: path.join(__dirname, 'assets/W.A.C.ico')
+    });
+
+    // on affiche le contenu de l'index.html
+    win.loadFile(htmlToLoad);
+    // supprime la barre de menu au top
+    win.menuBarVisible = false;
+    // met la fenêtre en grand écran
+    win.maximize();
+  }
+
+  // quand l'app est prête
+  app.whenReady().then(() => {
+    // on créer la fenêtre
+    createWindow();
+
+    // si y a aucune fenêtre ouverte, on en créer une
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
   });
 
-  // on affiche le contenu de l'index.html
-  win.loadFile('index.html');
-  // supprime la barre de menu au top
-  win.menuBarVisible = false;
-  // met la fenêtre en grand écran
-  win.maximize();
-}
+  // gestion fermeture de l'app sur Mac
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+  });
 
-// quand l'app est prête
-app.whenReady().then(() => {
-  // on créer la fenêtre
-  createWindow();
-
-  // si y a aucune fenêtre ouverte, on en créer une
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  })
-});
-
-// gestion fermeture de l'app sur Mac
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
 });
--- preload.js
+++ preload.js
@@ -1,1 +1,144 @@
+const fs = require('fs');
 
+function loadWebhooks() {
+  var leftMenu = document.querySelector("#left-menu");
+
+  if (leftMenu.childElementCount > 0) {
+    while (leftMenu.firstChild) {
+      leftMenu.removeChild(leftMenu.lastChild);
+    }
+  }
+
+  fs.readFile('webhooks-stack.json', 'utf-8', (err, data) => {
+    var list = JSON.parse(data);
+
+    list.forEach(webhook => {
+
+      var img = document.createElement("img");
+      img.setAttribute("src", webhook.pp);
+      img.setAttribute("alt", "webhook profile picture");
+      img.setAttribute("class", "pp-webhook rounded-circle");
+
+      var divImg = document.createElement("div");
+      divImg.setAttribute("class", "col-2");
+      divImg.appendChild(img);
+
+      var nameSpan = document.createElement("span");
+      nameSpan.innerHTML = webhook.name;
+
+      var name = document.createElement("div");
+      name.setAttribute("class", "col user-select-none text-cut");
+      name.appendChild(nameSpan);
+
+      var divItem = document.createElement("div");
+      divItem.setAttribute("class", "row align-items-start");
+      divItem.appendChild(divImg);
+      divItem.appendChild(name);
+
+      var a = document.createElement("a");
+      a.setAttribute("class", "nav-link link-light");
+      a.setAttribute("aria-current", "page");
+      a.setAttribute("id", `item${webhook.id}`);
+      a.appendChild(divItem);
+      a.onclick = () => {
+        var itemSelected = a.id;
+        var oldItem = document.querySelector(".active");
+        var newItem = document.querySelector(`#${itemSelected}`);
+
+        oldItem.setAttribute("class", "nav-link link-light");
+        newItem.setAttribute("class", "nav-link active");
+      }
+
+      a.onmouseover = () => {
+        var width = a.querySelector("span").getBoundingClientRect().width;
+        if (width > 175) {
+
+        }
+      }
+
+      var li = document.createElement("li");
+      li.setAttribute("class", "nav-item");
+      li.appendChild(a);
+
+      leftMenu.appendChild(li);
+    });
+    leftMenu.firstElementChild.querySelector("a").setAttribute("class", "nav-link active");
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector("body");
+
+  var form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    function errMsg(text) {
+      var zone = document.getElementById('errMsg');
+
+      if (zone.childElementCount > 0) {
+        while (zone.firstChild) {
+          zone.removeChild(zone.lastChild);
+        }
+      }
+
+      var button = document.createElement("button");
+      button.setAttribute("type", "button");
+      button.setAttribute("class", "btn-close");
+      button.setAttribute("data-bs-dismiss", "alert");
+      button.setAttribute("aria-label", "Close");
+
+      var textAlert = document.createElement("div");
+      textAlert.setAttribute("class", "alert alert-danger alert-dismissible fade show mt-3");
+      textAlert.setAttribute("role", "alert");
+      textAlert.innerHTML = text;
+      textAlert.appendChild(button);
+
+      zone.appendChild(textAlert);
+    }
+
+    if (form.URL.value) {
+      const webhook = form.URL.value;
+      fetch(webhook).then(res => res.json()).then(json => {
+        if(json.type && json.id && json.name && json.channel_id && json.guild_id && json.token){
+          fs.readFile('webhooks-stack.json', 'utf-8', (err, data) => {
+            if (err) console.error(err);
+
+            var list = JSON.parse(data);
+
+            if (!list.find(webhook => webhook.id === json.id)) {
+              var webhookProfil = {
+                id: json.id,
+                name: json.name,
+                pp: json.avatar!==null?`https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png`:"https://discord.com/assets/1f0bfc0865d324c2587920a7d80c609b.png",
+                url: webhook
+              }
+              list.push(webhookProfil);
+              var result = JSON.stringify(list);
+
+              fs.writeFile('webhooks-stack.json', result, 'utf-8', ()=>{});
+              if (body.id === "starter") {
+                document.location.href = "manager.html";
+              } else if (body.id === "manager") {
+                loadWebhooks();
+              }
+            } else {
+              errMsg("This webhook already exist.");
+            }
+          });
+        } else {
+          errMsg("You have to paste a valid webhook URL.");
+        }
+      }).catch(() => {
+        errMsg("You have to paste the webhook URL.");
+      });
+    }
+  });
+
+
+  if (body.id === "manager") {
+
+    loadWebhooks();
+
+  }
+});
